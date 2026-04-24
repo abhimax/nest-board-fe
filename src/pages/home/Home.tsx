b4475eb726945d5de74465740bfe51cc7e65bf3d
Abhiman Ranaweera <abhiman.max@gmail.com>
2026-04-24 17:51:16 +0530

    feat: enter to the tailwind grid intro

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,12 @@
-import { HeroSection } from './components/HeroSection'
-import { PropertyCard } from '@/components/common/PropertyCard'
+import { HeroSection } from "./components/HeroSection"
+import { PropertyCard } from "@/components/common/PropertyCard"
 export function Home() {
   return (
     <>
       <HeroSection />
+      {/* <div className="grid grid-cols-3 gap-3"> */}
       <PropertyCard />
+      {/* </div> */}
     </>
   )
 }
