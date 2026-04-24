c33b45d491e1d5fe67526845481b860744208ef6
Abhiman Ranaweera <abhiman.max@gmail.com>
2026-04-24 16:46:30 +0530

    fix: formatting card

--- src/components/common/PropertyCard.tsx
+++ src/components/common/PropertyCard.tsx
@@ -1,15 +1,19 @@
 export function PropertyCard() {
-
   return (
     <div>
       {/* Background image */}
-      <img src={'https://images.unsplash.com/photo-1545324418-cc1a3fa10c00?w=400&h=500&fit=crop'} alt={'title'} />
+      <img
+        src={
+          "https://images.unsplash.com/photo-1545324418-cc1a3fa10c00?w=400&h=500&fit=crop"
+        }
+        alt={"title"}
+      />
       {/* Rating badge */}
-    <span>*</span>
+      <span>*</span>
 
       {/* Bottom info */}
       <div>
-       <span>House</span>
+        <span>House</span>
         <h3>Green Villa Colombo</h3>
         <p>Colombo, Sri Lanka</p>
         <p>
