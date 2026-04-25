eff78a75f930f81ae6e351d9c2d5cf3f1712faa1
Abhiman Ranaweera <abhiman.max@gmail.com>
2026-04-25 16:59:22 +0530

    feat: react router v7 intro

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router"
 import { Home } from "./pages/home/Home"
+import { Property } from "./pages/property/Property"
 
 export function App() {
   return (
-    <Home/>
+    <BrowserRouter>
+      {/* Navigation Links */}
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </nav>
+
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<Property />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 export default App
---
+++ src/pages/property/PropertyDetails.tsx
@@ -0,0 +1,10 @@
+export function Property() {
+  return (
+    <div className="flex min-h-screen flex-col items-center py-2">
+      <h1 className="text-7xl text-orange-500">Property Details</h1>
+      <p className="mt-7 text-3xl font-light text-neutral-700">
+        This is a simple property page.
+      </p>
+    </div>
+  )
+}
