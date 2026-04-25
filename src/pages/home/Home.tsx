221cd24ce673429679c6736a3e67230bfefc1e89
Abhiman Ranaweera <abhiman.max@gmail.com>
2026-04-25 05:45:55 +0530

    feat: property list component

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,18 +1,11 @@
 import { HeroSection } from "./components/HeroSection"
-import { PropertyCard } from "@/components/common/PropertyCard"
 import { properties } from "@/data/properties"
+import { PropertyList } from "./components/PropertyList"
 export function Home() {
   return (
     <>
       <HeroSection />
-      <div className="grid grid-cols-3 gap-3">
-        {properties.map((property) => (
-          <PropertyCard
-            key={property.id}
-            {...property} // Spread operator to pass all property fields as props
-          />
-        ))}
-      </div>
+      <PropertyList properties={properties} />
     </>
   )
 }
