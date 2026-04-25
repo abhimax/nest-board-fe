92c8ea278abdec1bc3da9cebb92c8ed7331190c6
Abhiman Ranaweera <abhiman.max@gmail.com>
2026-04-25 07:22:44 +0530

    featch: search query completed

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,22 +1,28 @@
+import { useState } from "react"
 import { HeroSection } from "./components/HeroSection"
 import { properties } from "@/data/properties"
 import { PropertyList } from "./components/PropertyList"
 import { SearchFilters } from "./components/SearchFilters"
-import { useState } from "react"
+
 export function Home() {
   const [searchQuery, setSearchQuery] = useState("")
 
-  // Function that gets triggered when search changes
-  const handleSearchChange = (query: string) => {
-    setSearchQuery(query) // Update the local state with the new search query
-    console.log("Search query changed:", query) // Log the updated query
-  }
+  const filteredProperties = properties.filter((p) => {
+    return (
+      searchQuery === "" ||
+      p.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      p.location.toLowerCase().includes(searchQuery.toLowerCase())
+    )
+  })
+
   return (
     <>
       <HeroSection />
-      <SearchFilters onSearchChange={handleSearchChange} />
-      <h3 className="text-center">{searchQuery}</h3>
-      <PropertyList properties={properties} />
+      <SearchFilters
+        searchQuery={searchQuery}
+        onSearchChange={setSearchQuery}
+      />
+      <PropertyList properties={filteredProperties} />
     </>
   )
 }
