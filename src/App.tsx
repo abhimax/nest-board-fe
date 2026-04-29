6c62b4281911ddcfab9738f21c4f5879b2768bfd
Abhiman Ranaweera <abhiman.max@gmail.com>
2026-04-29 06:43:23 +0530

    feat: update route pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import { BrowserRouter, Link, Route, Routes } from "react-router"
 import { Home } from "./pages/home/Home"
 import { Property } from "./pages/property/PropertyDetails"
+import { Map } from "./pages/map/Map"
 
 export function App() {
   return (
     <BrowserRouter>
       {/* Navigation Links */}
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
+        <Link to="/">Explore</Link>
+        <Link to="/about">Property Details</Link>
+        <Link to="/map">Map View</Link>
       </nav>
 
       {/* Routes */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<Property />} />
+        <Route path="/map" element={<Map />} />
       </Routes>
     </BrowserRouter>
   )
